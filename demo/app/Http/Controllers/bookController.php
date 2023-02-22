4323a5044b3781b94a0caf2bdd312ea1bb8bb8bd
JustinBryan69 <jtin253@gmail.com>
2023-02-22 13:40:02 +0800

    add library

---
+++ demo/app/Http/Controllers/bookController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class bookController extends Controller
+{
+    public function index()
+    {
+      
+
+        return 'Books';
+    }
+}
---
+++ demo/database/factories/bookFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\book>
+ */
+class bookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title'=>fake()->word(),
+            'totalPages'=>fake()->randomDigit(),
+            'rating'=>fake()->randomDigit(),
+            'isbn'=>fake()->randomDigit(),
+            'publishDate'=>fake()->randomDigit(),
+            ];
+    }
+}
